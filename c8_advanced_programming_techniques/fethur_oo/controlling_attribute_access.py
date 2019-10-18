63440f790cf8c0d2ef8a0c4370666772615d5254
Hack Chyson <chyson@aliyun.com>
2019-10-18 15:48:58 +0800

    controlling attributes access;

--- c8_advanced_programming_techniques/fethur_oo/controlling_attribute_access.py
+++ c8_advanced_programming_techniques/fethur_oo/controlling_attribute_access.py
@@ -42,6 +42,39 @@ class Const:
         raise AttributeError("'{}' object has no attribute '{}'".format(self.__class__.__name__, item))
 
 
+class Image:
+    def __init__(self, width, height, filename="", background="#FFFFFF"):
+        self.filename = filename
+        self.__background = background
+        self.__data = {}
+        self.__width = width
+        self.__height = height
+        self.__colors = {self.__background}
+
+    # This is easy to code but could become tedious if there are a lot of read-only
+    # properties.
+    @property
+    def width(self):
+        return self.__width
+
+    # If we attempt to access an object’s attribute and the attribute is not found,
+    # Python will call the __getattr__() method (providing it is implemented, and
+    # that we have not reimplemented __getattribute__() ), with the name of the
+    # attribute as a parameter. Implementations of __getattr__() must raise an
+    # AttributeError exception if they do not handle the given attribute.
+    def __getattr__(self, name):
+        if name == 'colors':
+            return set(self.__colors)
+        classname = self.__class__.__name__
+        if name in frozenset({'background', 'width', 'height'}):
+            # For private attributes (those whose
+            # name begins with two leading underscores), the name is mangled to have the
+            # form _className__attributeName , so we must account for this when retrieving
+            # the attribute’s value from the object’s private dictionary.
+            return self.__dict__['_{classname}__{name}'.format(**locals())]
+        raise AttributeError("'{classname}' object has no attribute '{name}'".format(**locals()))
+
+
 if __name__ == '__main__':
     ord = Ord()
     print(ord.a)
@@ -57,3 +90,8 @@ if __name__ == '__main__':
     print(Const.min, Const.max)
     Offset = collections.namedtuple('_', 'id name description')(*range(3))
     print(Offset.id, Offset.name, Offset.description)
+
+    image = Image(10, 10)
+    print(image.__dict__)
+    print(image.colors)
+    print(image.height)
