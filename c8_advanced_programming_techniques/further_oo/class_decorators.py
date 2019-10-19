341ec3e14124e096672ecc061fc32886fdc45a37
Hack Chyson <chyson@aliyun.com>
2019-10-19 19:02:35 +0800

    descriptor;

---
+++ c8_advanced_programming_techniques/further_oo/class_decorators.py
@@ -0,0 +1,40 @@
+from chyson.decorators.decorators import delegate, complete_comparisons
+
+_identitiy = lambda x: x
+
+
+@delegate('__list', ('pop', '__delitem__', '__getitem__', '__iter__', '__reversed__', '__len__', '__str__'))
+class SortedList:
+    def __init__(self, sequence=None, key=None):
+        self.__key = key or _identitiy
+        assert hasattr(self.__key, '__call__')
+        if sequence is None:
+            self.__list = []
+        elif isinstance(sequence, SortedList) and sequence.key == self.__key:
+            self.__list = sequence.__list[:]
+        else:
+            self.__list = sorted(list(sequence), key=self.__key)
+
+
+@complete_comparisons
+class FuzzyBool:
+    def __init__(self, value=0.0):
+        self.__value = value if 0.0 <= value <= 1.0 else 0.0
+
+    def __lt__(self, other):
+        return self.__value < other.__value
+
+
+if __name__ == '__main__':
+    sl = SortedList([1, 2, 3])
+    print(sl)
+    print(sl.pop())
+    print(len(sl))
+    print(list(reversed(sl)))
+    for i in sl:
+        print(i)
+
+    fb1 = FuzzyBool(0.5)  # todo TypeError: 'NoneType' object is not callable
+
+    # fb2 = FuzzyBool(.7)
+    # print(fb1 < fb2, fb1 <= fb2, fb2 == fb2, fb1 != fb2, fb1 > fb2, fb1 >= fb2)
