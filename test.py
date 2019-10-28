f7714fa575116c3ba134b19da652f52d495437c0
Hack Chyson <chyson@aliyun.com>
2019-10-28 16:14:22 +0800

    A  c8_advanced_programming_techniques/further_oo/abc.py  M test.py

--- test.py
+++ test.py
@@ -12,9 +12,3 @@
 # if __name__ == '__main__':
 #     hello(10)
 #
-# from c10_advanced_programming_techniques.function_annotations import range_from_one
-
-import sys
-from chyson.os.magic.magic_file_type import get_file_types
-
-get_file_types(sys.argv[1:])
