d429268f6bd987fbbe5a9e8661d4d8143f6d5b56
Mike Chyson <mile.chyson@gmail.com>
2021-02-26 09:29:22 +0800

    database

--- c12_database/dvds_sql.py
+++ c12_database/dvds_sql.py
@@ -126,158 +126,109 @@ def get_director_id(db, director):
 
 
 def edit_dvd(db):
-    title, identity = find_dvd(db, "edit")
+    # Prepare data
+    title, identity = find_dvd(db, 'edit')
     if title is None:
         return
-    title = Console.get_string("Title", "title", title)
+    title = Console.get_string('Title', 'title', title)
     if not title:
         return
 
-    # Get the old data
+    # Get old data
     cursor = db.cursor()
-    cursor.execute("SELECT dvds.year, dvds.duration, directors.name "
-                   "FROM dvds, directors "
-                   "WHERE dvds.director_id = directors.id AND "
-                   "dvds.id=:id", dict(id=identity))
+    cursor.execute(
+        'SELECT dvds.year, dvds.duration, directors.name '
+        'FROM dvds, directors '
+        'WHERE dvds.director_id = directors.id AND '
+        'dvds.id=:id',
+        dict(id=identity)
+    )
     year, duration, director = cursor.fetchone()
 
-    # Get the new data
-    director = Console.get_string("Director", "director", director)
+    director = Console.get_string('Director', 'director', director)
     if not director:
         return
-    year = Console.get_integer("Year", "year", year, 1896,
-                               datetime.date.today().year)
-    duration = Console.get_integer("Duration (minutes)", "minutes",
-                                   duration, minimum=0, maximum=60 * 48)
-
-    # Edit
+    year = Console.get_integer('Year', 'year', year, 1896, datetime.date.today().year)
+    duration = Console.get_integer('Duration (minutes)', 'minutes', duration, 0, 60 * 48)
     director_id = get_and_set_director(db, director)
-    cursor.execute("UPDATE dvds SET title=:title, year=:year, "
-                   "duration=:duration, director_id=:director_id "
-                   "WHERE id=:identity", locals())
+    cursor.execute(
+        'UPDATE dvds SET title=:title, year=:year, '
+        'duration=:duration, director_id=:director_id '
+        'WHERE id=:identity',
+        locals()
+    )
     db.commit()
+
+
+def find_dvd(db, message):
+    message = '(Start of) title to ' + message
+    cursor = db.cursor()
+    while True:
+        start = Console.get_string(message, 'title')
+        if not start:
+            return None, None
+        cursor.execute(
+            'SELECT title, id FROM dvds '
+            'WHERE title LIKE ? ORDER BY title',
+            (start + '%',)
+        )
+        records = cursor.fetchall()
+        if len(records) == 0:
+            print('There are no dvds starting with', start)
+            continue
+        elif len(records) == 1:
+            return records[0]
+        elif len(records) > DISPLAY_LIMIT:
+            print(f'Too many dvds ({len(records)}) start with {start};'
+                  'try entering more of the title')
+            continue
+        else:
+            for i, record in enumerate(records, start=1):
+                print(f'{i}: {record[0]}')
+            which = Console.get_integer('Number (or ) to cancel)', 'number', 1, len(records))
+            return records[which - 1] if which != 0 else None, None
 
 
 def list_dvds(db):
     cursor = db.cursor()
-    sql = ("SELECT dvds.title, dvds.year, dvds.duration, "
-           "directors.name FROM dvds, directors "
-           "WHERE dvds.director_id = directors.id")
+    sql = (
+        'SELECT dvds.title, dvds.year, dvds.duration, '
+        'directors.name FROM dvds, directors '
+        'WHERE dvds.director_id = directors.id'
+    )
     start = None
     if dvd_count(db) > DISPLAY_LIMIT:
-        start = Console.get_string("List those starting with "
-                                   "[Enter=all]", "start")
-        sql += " AND dvds.title LIKE ?"
-    sql += " ORDER BY dvds.title"
+        start = Console.get_string('List those starting with [Enter=all]', 'start')
+        sql += ' AND dvds.title LIKE ?'
+    sql += ' ORDER BY dvds.title'
     print()
     if start is None:
         cursor.execute(sql)
     else:
-        cursor.execute(sql, (start + "%",))
+        cursor.execute(sql, (start + '%',))
+    print('{:<20} {:<10} {:<20} {:<20}'.format(
+        'Title', 'Year', 'Duration (minutes)', 'Director'
+    ))
+    print('-' * 80)
     for record in cursor:
-        print("{0[0]} ({0[1]}) {0[2]} minutes, by {0[3]}".format(
-            record))
-
-
-def list_directors(db):
-    cursor = db.cursor()
-    cursor.execute("SELECT COUNT(*) FROM directors")
-    count = cursor.fetchone()[0]
-    sql = "SELECT name FROM directors"
-    start = None
-    if count > DISPLAY_LIMIT:
-        start = Console.get_string("List those starting with "
-                                   "[Enter=all]", "start")
-        sql += " WHERE name LIKE ?"
-    sql += " ORDER BY name"
-    print()
-    if start is None:
-        cursor.execute(sql)
-    else:
-        cursor.execute(sql, (start + "%",))
-    for fields in cursor:
-        print(fields[0])
-
-
-def remove_dvd(db):
-    title, identity = find_dvd(db, "remove")
-    if title is None:
-        return
-    ans = Console.get_bool("Remove {0}?".format(title), "no")
-    if ans:
-        cursor = db.cursor()
-        cursor.execute("DELETE FROM dvds WHERE id=?", (identity,))
-        db.commit()
-
-
-def import_(db):
-    filename = Console.get_string("Import from", "filename")
-    if not filename:
-        return
-    try:
-        tree = xml.etree.ElementTree.parse(filename)
-    except (EnvironmentError,
-            xml.parsers.expat.ExpatError) as err:
-        print("ERROR:", err)
-        return
-
-    cursor = db.cursor()
-    cursor.execute("DELETE FROM directors")
-    cursor.execute("DELETE FROM dvds")
-
-    for element in tree.findall("dvd"):
-        get_and_set_director(db, element.get("director"))
-    for element in tree.findall("dvd"):
-        try:
-            year = int(element.get("year"))
-            duration = int(element.get("duration"))
-            title = element.text.strip()
-            director_id = get_director_id(db, element.get("director"))
-            cursor.execute("INSERT INTO dvds "
-                           "(title, year, duration, director_id) "
-                           "VALUES (?, ?, ?, ?)",
-                           (title, year, duration, director_id))
-        except ValueError as err:
-            db.rollback()
-            print("ERROR:", err)
-            break
-    else:
-        db.commit()
-    count = dvd_count(db)
-    print("Imported {0} dvd{1}".format(count, Util.s(count)))
+        print(f'{record[0]:<20} {record[1]:<10} {record[2]:<20} {record[3]:<20}')
 
 
 def dvd_count(db):
     cursor = db.cursor()
-    cursor.execute("SELECT COUNT(*) FROM dvds")
+    cursor.execute('SELECT COUNT(*) FROM dvds')
     return cursor.fetchone()[0]
 
 
-def export(db):
-    TITLE, YEAR, DURATION, DIRECTOR = range(4)
-    filename = os.path.join(tempfile.gettempdir(), "dvds.xml")
-    cursor = db.cursor()
-    cursor.execute("SELECT dvds.title, dvds.year, dvds.duration, "
-                   "directors.name FROM dvds, directors "
-                   "WHERE dvds.director_id = directors.id "
-                   "ORDER BY dvds.title ")
-    try:
-        with open(filename, "w", encoding="utf8") as fh:
-            fh.write('<?xml version="1.0" encoding="UTF-8"?>\n')
-            fh.write("<dvds>\n")
-            for record in cursor:
-                fh.write('<dvd year="{0}" duration="{1}" '
-                         'director={2}>'.format(
-                    record[YEAR], record[DURATION],
-                    xml.sax.saxutils.quoteattr(record[DIRECTOR])))
-                fh.write(xml.sax.saxutils.escape(record[TITLE]))
-                fh.write("</dvd>\n")
-            fh.write("</dvds>\n")
-    except EnvironmentError as err:
-        print(err)
-    count = dvd_count(db)
-    print("exported {0} dvd{1} to {2}".format(
-        count, Util.s(count), filename))
+def remove_dvd(db):
+    title, identity = find_dvd(db, 'remove')
+    if title is None:
+        return
+    ans = Console.get_bool(f'Remove {title}?', 'no')
+    if ans:
+        cursor = db.cursor()
+        cursor.execute('DELETE FROM dvds WHERE id=?', (identity,))
+        db.commit()
 
 
 def quit_(db):
@@ -285,36 +236,73 @@ def quit_(db):
         count = dvd_count(db)
         db.commit()
         db.close()
-        print("Saved {0} dvd{1}".format(count, Util.s(count)))
+        print(f'Saved {count} dvds{Util.s(count)}')
     sys.exit()
 
 
-def find_dvd(db, message):
-    message = "(Start of) title to " + message
+def export(db):
+    TITLE, YEAR, DURATION, DIRECTOR = range(4)
+    filename = os.path.join(os.path.dirname(__file__), 'dvds.xml')
+
     cursor = db.cursor()
-    while True:
-        start = Console.get_string(message, "title")
-        if not start:
-            return (None, None)
-        cursor.execute("SELECT title, id FROM dvds "
-                       "WHERE title LIKE ? ORDER BY title",
-                       (start + "%",))
-        records = cursor.fetchall()
-        if len(records) == 0:
-            print("There are no dvds starting with", start)
-            continue
-        elif len(records) == 1:
-            return records[0]
-        elif len(records) > DISPLAY_LIMIT:
-            print("Too many dvds ({0}) start with {1}; try entering "
-                  "more of the title".format(len(records), start))
-            continue
-        else:
-            for i, record in enumerate(records):
-                print("{0}: {1}".format(i + 1, record[0]))
-            which = Console.get_integer("Number (or 0 to cancel)",
-                                        "number", minimum=1, maximum=len(records))
-            return records[which - 1] if which != 0 else (None, None)
+    cursor.execute(
+        'SELECT dvds.title, dvds.year, dvds.duration, '
+        'directors.name FROM dvds, directors '
+        'WHERE dvds.director_id = directors.id '
+        'ORDER BY dvds.title'
+    )
+    try:
+        with open(filename, 'w', encoding='utf8') as fh:
+            fh.write('<?xml version="1.0" encoding="UTF-8"?>\n')
+            fh.write("<dvds>\n")
+            for record in cursor:
+                fh.write(f'<dvd year="{record[YEAR]}" duration="{record[DURATION]}" '
+                         f'director={xml.sax.saxutils.quoteattr(record[DIRECTOR])}>')
+                fh.write(xml.sax.saxutils.escape(record[TITLE]))
+                fh.write('</dvd>\n')
+            fh.write('</dvds>\n')
+    except EnvironmentError as  err:
+        print(err)
+    count = dvd_count(db)
+    print(f'Exported {count} dvd{Util.s(count)} to {filename}')
+
+
+def import_(db):
+    filename = Console.get_string('Import from', 'filename')
+    if not filename:
+        return
+    try:
+        tree = xml.etree.ElementTree.parse(filename)
+    except (EnvironmentError, xml.parsers.expat.ExpatError) as err:
+        print('ERROR:', err)
+        return
+
+    cursor = db.cursor()
+    cursor.execute('DELETE FROM directors')
+    cursor.execute('DELETE FROM dvds')
+
+    for element in tree.findall('dvd'):
+        get_and_set_director(db, element.get('director'))
+    for element in tree.findall('dvd'):
+        try:
+            year = int(element.get('year'))
+            duration = int(element.get('duration'))
+            title = element.text.strip()
+            director_id = get_and_set_director(db, element.get('director'))
+            cursor.execute(
+                'INSERT INTO dvds '
+                '(title, year, duration, director_id) '
+                'VALUES (?, ?, ?, ?)',
+                (title, year, duration, director_id)
+            )
+        except ValueError as err:
+            db.rollback()
+            print('ERROR:', err)
+            break
+    else:
+        db.commit()
+    count = dvd_count(db)
+    print(f'Imported {count} dvd{Util.s(count)}')
 
 
 if __name__ == '__main__':
